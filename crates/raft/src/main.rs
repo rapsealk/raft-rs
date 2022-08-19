8fc073f898b8845186180c5a3355e8c48d0afe8f
rapsealk <piono623@naver.com>
2022-08-19 21:27:56 +0900

    .

--- crates/raft/src/main.rs
+++ crates/raft/src/main.rs
@@ -1,3 +1,28 @@
+use std::thread;
+
+enum ConsensusState {
+    Follower,
+    Candidate,
+    Leader,
+}
+
+fn spawn_timer_thread() -> thread::JoinHandle<i32> {
+    return thread::spawn(move || {
+        println!("TimerThread!");
+        return 0;
+    });
+}
+
 fn main() {
     println!("Hello, world!");
+    let initial_state = ConsensusState::Follower;
+    let timer_thread_join_handler = spawn_timer_thread();
+
+    match initial_state {
+        ConsensusState::Follower => println!("Initial state: Follower"),
+        ConsensusState::Candidate => println!("Initial state: Candidate"),
+        ConsensusState::Leader => println!("Initial state: Leader"),
+    }
+
+    timer_thread_join_handler.join().expect("panic!");
 }
